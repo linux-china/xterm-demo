4ff2065b92101cc4deac4302d61caa2f6f80f02b
linux_china <libing.chen@gmail.com>
2020-01-15 15:43:18 -0800

    connect xterm with RSocket

--- src/main/javascript/console.js
+++ src/main/javascript/console.js
@@ -1,30 +1,111 @@
+//xterm imports
 import {Terminal} from 'xterm';
 import "xterm/css/xterm.css";
+//rsocket imports
+import {
+    BufferEncoders,
+    encodeAndAddWellKnownMetadata,
+    MESSAGE_RSOCKET_COMPOSITE_METADATA,
+    MESSAGE_RSOCKET_ROUTING,
+    RSocketClient,
+} from 'rsocket-core';
+import RSocketWebSocketClient from 'rsocket-websocket-client';
+import {FlowableProcessor} from "rsocket-flowable";
 
+// current command line
+let commandLine = '';
+// command flux
+let commandFlux = new FlowableProcessor({});
+
+// trigger command
+function triggerCommand(command) {
+    if (command.trim().length > 0) {
+        commandFlux.onNext({data: new Buffer(command)});
+    }
+}
+
+//output remote result
+function outputRemoteResult(payload) {
+    if (payload.data != null && payload.data.length > 0) {
+        term.write('\b \b');
+        term.write(new TextDecoder("utf-8").decode(payload.data));
+        term.prompt();
+    }
+}
+
+//initialize xterm
 let term = new Terminal();
 term.open(document.getElementById('terminal'));
 term.prompt = () => {
     term.write('\r\n$');
 };
-term.writeln('Welcome to xterm.');
+term.writeln('Welcome xterm with RSocket.');
 term.prompt();
 term.focus();
-// Move curr_line outside of async scope.
-let currLine = '';
 
-function triggerCommand(line) {
-    console.log("command line: " + line)
-}
-
+//term key event for Input & Enter & Backspace
 term.onKey((ev) => {
     let key = ev.key;
     let keyboardEvent = ev.domEvent;
     if (keyboardEvent.key === "Enter") {
         term.prompt();
-        triggerCommand(currLine);
-        currLine = '';
+        triggerCommand(commandLine);
+        commandLine = '';
+    } else if (keyboardEvent.key === "Backspace") {
+        if (commandLine.length > 0) {
+            term.write('\b \b');
+            commandLine = commandLine.substr(0, commandLine.length - 1);
+        }
     } else {
-        currLine += key;
+        commandLine += key;
         term.write(key);
     }
 });
+
+//initialize RSocket
+const maxRSocketRequestN = 2147483647;
+const keepAlive = 60000;
+const lifetime = 180000;
+const dataMimeType = "text/plain";
+const metadataMimeType = MESSAGE_RSOCKET_COMPOSITE_METADATA.string;
+
+const rsocketClient = new RSocketClient({
+    setup: {
+        keepAlive,
+        lifetime,
+        dataMimeType,
+        metadataMimeType,
+    },
+    transport: new RSocketWebSocketClient(
+            {wsCreator: () => new WebSocket('ws://localhost:8080/rsocket'), debug: true},
+            BufferEncoders,
+    ),
+});
+
+function routingKey(key) {
+    let buffer = Buffer.alloc(1 + key.length);
+    buffer.writeInt8(key.length, 0);
+    buffer.write(key, 1);
+    return buffer
+}
+
+// Open the connection
+rsocketClient.connect().then(rsocket => {
+    rsocket.requestChannel(commandFlux).subscribe({
+        onComplete: () => console.log('Terminal completed'),
+        onError: error => console.error(`Communication error:${error.message}`),
+        onNext: outputRemoteResult,
+        onSubscribe: sub => sub.request(maxRSocketRequestN),
+    });
+    //first payload with routing key
+    commandFlux.onNext(
+            {
+                data: new Buffer(''),
+                metadata: encodeAndAddWellKnownMetadata(
+                        Buffer.alloc(0),
+                        MESSAGE_RSOCKET_ROUTING,
+                        routingKey('xterm.shell'),
+                )
+            });
+});
+
