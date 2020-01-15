7dc0250ecf14203d8776468682b7fe7be3e4296e
linux_china <libing.chen@gmail.com>
2020-01-15 14:06:57 -0800

    RsocketXtermBackendApplication

---
+++ src/main/java/org/mvnsearch/rsocket/xterm/backend/RsocketXtermBackendApplication.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.rsocket.xterm.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketXtermBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsocketXtermBackendApplication.class, args);
+	}
+
+}
