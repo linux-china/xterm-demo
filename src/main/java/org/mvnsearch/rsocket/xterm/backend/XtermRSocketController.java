4734517d272c40f03817a1a86f170563b496b9e2
linux_china <libing.chen@gmail.com>
2020-01-15 14:04:53 -0800

    xterm rsocket controller

---
+++ src/main/java/org/mvnsearch/rsocket/xterm/backend/XtermRSocketController.java
@@ -0,0 +1,27 @@
+package org.mvnsearch.rsocket.xterm.backend;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * xterm rsocket controller
+ *
+ * @author linux_china
+ */
+@Controller
+public class XtermRSocketController {
+
+    @MessageMapping("xterm.shell")
+    public Flux<String> shell(Flux<String> commands) {
+        return commands.map(command -> {
+            return "command: " + command;
+        });
+    }
+
+    @MessageMapping("xterm.command")
+    public Mono<String> command(String command) {
+        return Mono.just("command:" + command);
+    }
+}
