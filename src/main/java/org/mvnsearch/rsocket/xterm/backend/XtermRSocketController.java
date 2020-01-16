e09dd283fc45f1940f34978ad05d4347b2b971ba
linux_china <libing.chen@gmail.com>
2020-01-15 16:10:24 -0800

    add executeCommand

--- src/main/java/org/mvnsearch/rsocket/xterm/backend/XtermRSocketController.java
+++ src/main/java/org/mvnsearch/rsocket/xterm/backend/XtermRSocketController.java
@@ -1,5 +1,6 @@
 package org.mvnsearch.rsocket.xterm.backend;
 
+import org.springframework.lang.Nullable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
@@ -15,13 +16,25 @@ public class XtermRSocketController {
 
     @MessageMapping("xterm.shell")
     public Flux<String> shell(Flux<String> commands) {
-        return commands.map(command -> {
-            if (command.isEmpty()) {
-                return "";
-            } else {
-                return ">>> " + command;
-            }
-        });
+        return commands
+                .filter(data -> !data.trim().isEmpty())
+                .flatMap(data -> {
+                    String commandLine = data.trim();
+                    String command = commandLine;
+                    String params = null;
+                    if (commandLine.contains(" ")) {
+                        command = commandLine.substring(0, commandLine.indexOf(" "));
+                        params = commandLine.substring(commandLine.indexOf(" ") + 1).trim();
+                    }
+                    return executeCommand(command, params);
+                });
+    }
+
+    public Mono<String> executeCommand(String command, @Nullable String params) {
+        if (command.equalsIgnoreCase("info")) {
+            return Mono.just("xterm with RSocket: http://rsocket.io");
+        }
+        return Mono.just(">>> " + command);
     }
 
     @MessageMapping("xterm.command")
