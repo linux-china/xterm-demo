d48d8c6942348fb8162c88f94f6faf6b68c9596b
linux_china <libing.chen@gmail.com>
2020-01-16 16:49:53 -0800

    exclude some configurations from spring shell

--- src/main/java/org/mvnsearch/rsocket/xterm/backend/RsocketXtermBackendApplication.java
+++ src/main/java/org/mvnsearch/rsocket/xterm/backend/RsocketXtermBackendApplication.java
@@ -2,12 +2,14 @@ package org.mvnsearch.rsocket.xterm.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.jline.JLineShellAutoConfiguration;
+import org.springframework.shell.standard.commands.StandardCommandsAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JLineShellAutoConfiguration.class, StandardCommandsAutoConfiguration.class})
 public class RsocketXtermBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RsocketXtermBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RsocketXtermBackendApplication.class, args);
+    }
 
 }
