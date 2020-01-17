192987dcd50e75465ea707725538e1686179b8c3
linux_china <libing.chen@gmail.com>
2020-01-17 11:58:08 -0800

    remove excludes

--- src/main/java/org/mvnsearch/rsocket/xterm/backend/RsocketXtermBackendApplication.java
+++ src/main/java/org/mvnsearch/rsocket/xterm/backend/RsocketXtermBackendApplication.java
@@ -2,10 +2,8 @@ package org.mvnsearch.rsocket.xterm.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.shell.jline.JLineShellAutoConfiguration;
-import org.springframework.shell.standard.commands.StandardCommandsAutoConfiguration;
 
-@SpringBootApplication(exclude = {JLineShellAutoConfiguration.class, StandardCommandsAutoConfiguration.class})
+@SpringBootApplication
 public class RsocketXtermBackendApplication {
 
     public static void main(String[] args) {
