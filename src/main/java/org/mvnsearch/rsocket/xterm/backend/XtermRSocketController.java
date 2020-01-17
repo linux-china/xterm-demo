e4771fd16f44157a526c9ed1b369d15934a8994c
linux_china <libing.chen@gmail.com>
2020-01-17 12:20:53 -0800

    adjust structure

--- src/main/java/org/mvnsearch/rsocket/xterm/backend/XtermRSocketController.java
+++ src/main/java/org/mvnsearch/rsocket/xterm/backend/XtermRSocketController.java
@@ -1,10 +1,6 @@
 package org.mvnsearch.rsocket.xterm.backend;
 
-import org.jline.utils.AttributedString;
-import org.jline.utils.AttributedStyle;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.shell.Shell;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -15,26 +11,13 @@ import reactor.core.publisher.Mono;
  * @author linux_china
  */
 @Controller
-public class XtermRSocketController {
-    @Autowired
-    private Shell shell;
+public class XtermRSocketController extends XtermHandler {
 
     @MessageMapping("xterm.shell")
     public Flux<String> shell(Flux<String> commands) {
         return commands
                 .filter(data -> !data.trim().isEmpty())
-                .map(commandLine -> {
-                    Object result = this.shell.evaluate(() -> commandLine);
-                    String textOutput;
-                    if (result instanceof Exception) {
-                        textOutput = new AttributedString(result.toString(), AttributedStyle.DEFAULT.foreground(AttributedStyle.RED)).toAnsi();
-                    } else if (result instanceof AttributedString) {
-                        textOutput = ((AttributedString) result).toAnsi();
-                    } else {
-                        textOutput = result.toString();
-                    }
-                    return textOutput;
-                });
+                .map(this::executeCommand);
     }
 
     @MessageMapping("xterm.command")
