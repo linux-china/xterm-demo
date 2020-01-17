1c988c0e7d8625331a4925283c300aca18bb0122
linux_china <libing.chen@gmail.com>
2020-01-16 17:34:36 -0800

    add info

--- src/main/java/org/mvnsearch/rsocket/xterm/backend/MyCommands.java
+++ src/main/java/org/mvnsearch/rsocket/xterm/backend/MyCommands.java
@@ -1,5 +1,7 @@
 package org.mvnsearch.rsocket.xterm.backend;
 
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
@@ -10,6 +12,8 @@ import org.springframework.shell.standard.ShellMethod;
  */
 @ShellComponent
 public class MyCommands {
+    private static AttributedStyle RED_FONT_STYLE = AttributedStyle.DEFAULT.foreground(AttributedStyle.RED);
+    private static AttributedStyle GREEN_FONT_STYLE = AttributedStyle.DEFAULT.foreground(AttributedStyle.GREEN);
 
     @ShellMethod("Add two integers together.")
     public int add(int a, int b) {
@@ -22,7 +26,7 @@ public class MyCommands {
     }
 
     @ShellMethod("Display information.")
-    public String info() {
-        return "Xterm with RSocket! http://rsocket.io ";
+    public AttributedString info() {
+        return new AttributedString("Xterm with RSocket! http://rsocket.io ", GREEN_FONT_STYLE);
     }
 }
