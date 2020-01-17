a72acaf194612c13bd6d6f536e5234145e27f294
linux_china <libing.chen@gmail.com>
2020-01-17 12:25:12 -0800

    add websocket support

--- src/main/java/org/mvnsearch/rsocket/xterm/backend/XtermAutoConfiguration.java
+++ src/main/java/org/mvnsearch/rsocket/xterm/backend/XtermAutoConfiguration.java
@@ -1,14 +1,21 @@
 package org.mvnsearch.rsocket.xterm.backend;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.shell.ParameterResolver;
 import org.springframework.shell.jline.JLineShellAutoConfiguration;
 import org.springframework.shell.standard.commands.Help;
 import org.springframework.shell.standard.commands.StandardCommandsAutoConfiguration;
+import org.springframework.web.reactive.HandlerMapping;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Xterm auto configuration
@@ -21,5 +28,23 @@ public class XtermAutoConfiguration {
     @Bean
     public Help help(List<ParameterResolver> parameterResolvers) {
         return new Help(parameterResolvers);
+    }
+
+    @Bean
+    public XtermWSHandler xtermWSHandler() {
+        return new XtermWSHandler();
+    }
+
+    @Bean
+    public HandlerMapping xtermHandlerMapping(XtermWSHandler xtermWSHandler) {
+        Map<String, WebSocketHandler> map = new HashMap<>();
+        map.put("/ws/xterm", xtermWSHandler);
+        return new SimpleUrlHandlerMapping(map, -1);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public WebSocketHandlerAdapter handlerAdapter() {
+        return new WebSocketHandlerAdapter();
     }
 }
