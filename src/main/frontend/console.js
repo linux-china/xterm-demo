60ef174aa4c6b9a4e8360b80fed111b23f6ea426
linux_china <libing.chen@gmail.com>
2020-01-16 21:09:05 -0800

    adjust to web component

--- src/main/frontend/console.js
+++ src/main/frontend/console.js
@@ -5,17 +5,27 @@ import {FitAddon} from 'xterm-addon-fit';
 import {RSocketAddon} from './xterm-addon-rsocket';
 import "xterm/css/xterm.css";
 
-//initialize xterm
-let term = new Terminal();
-//set up prompt
-term.prompt = () => {
-    term.write('\r\n$');
-};
-//load addons
-term.loadAddon(new WebLinksAddon());
-term.loadAddon(new FitAddon());
-term.loadAddon(new RSocketAddon("ws://localhost:8080/rsocket"));
-term.open(document.getElementById('terminal'));
-term.writeln('Welcome xterm with RSocket.');
-term.prompt();
-term.focus();
+class XtermConsole extends HTMLElement {
+    constructor() {
+        super();
+        let rsocketUrl = this.getAttribute('rsocket');
+        let container = document.createElement('div');
+        this.append(container);
+        //initialize xterm
+        let term = new Terminal();
+        term.prompt = () => {
+            term.write('\r\n$');
+        };
+        //load addons
+        term.loadAddon(new WebLinksAddon());
+        term.loadAddon(new FitAddon());
+        term.loadAddon(new RSocketAddon(rsocketUrl));
+        term.open(container);
+        term.writeln('Welcome xterm with RSocket.');
+        term.prompt();
+        term.focus();
+        this.terminal = term;
+    }
+}
+
+window.customElements.define('xterm-console', XtermConsole);
