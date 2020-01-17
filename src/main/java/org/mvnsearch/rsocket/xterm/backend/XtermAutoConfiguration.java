9a7214f08b7c1f8eecd2d9727244b92db4907635
linux_china <libing.chen@gmail.com>
2020-01-17 11:58:19 -0800

    add EnableAutoConfiguration with excludes

--- src/main/java/org/mvnsearch/rsocket/xterm/backend/XtermAutoConfiguration.java
+++ src/main/java/org/mvnsearch/rsocket/xterm/backend/XtermAutoConfiguration.java
@@ -1,9 +1,12 @@
 package org.mvnsearch.rsocket.xterm.backend;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.shell.ParameterResolver;
+import org.springframework.shell.jline.JLineShellAutoConfiguration;
 import org.springframework.shell.standard.commands.Help;
+import org.springframework.shell.standard.commands.StandardCommandsAutoConfiguration;
 
 import java.util.List;
 
@@ -13,6 +16,7 @@ import java.util.List;
  * @author linux_china
  */
 @Configuration
+@EnableAutoConfiguration(exclude = {JLineShellAutoConfiguration.class, StandardCommandsAutoConfiguration.class})
 public class XtermAutoConfiguration {
     @Bean
     public Help help(List<ParameterResolver> parameterResolvers) {
