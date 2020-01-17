ecc4344c3e3eb6166dbe2ea75e651878015fa66a
linux_china <libing.chen@gmail.com>
2020-01-17 12:20:20 -0800

    Xterm handler

---
+++ src/main/java/org/mvnsearch/rsocket/xterm/backend/XtermHandler.java
@@ -0,0 +1,29 @@
+package org.mvnsearch.rsocket.xterm.backend;
+
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.Shell;
+
+/**
+ * Xterm handler: execute the commands from xterm.js
+ *
+ * @author linux_china
+ */
+public class XtermHandler {
+    @Autowired
+    private Shell shell;
+
+    public String executeCommand(String commandLine) {
+        Object result = this.shell.evaluate(() -> commandLine);
+        String textOutput;
+        if (result instanceof Exception) {
+            textOutput = new AttributedString(result.toString(), AttributedStyle.DEFAULT.foreground(AttributedStyle.RED)).toAnsi();
+        } else if (result instanceof AttributedString) {
+            textOutput = ((AttributedString) result).toAnsi();
+        } else {
+            textOutput = result.toString();
+        }
+        return textOutput;
+    }
+}
